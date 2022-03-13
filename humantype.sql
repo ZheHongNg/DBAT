62e00f76e59b0db3ceeaeecb187b8a140d341f7c
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-14 00:34:23 +0800

    Update humantype.sql

--- humantype.sql
+++ humantype.sql
@@ -1,2 +1,11 @@
+--paleolitik
+
 INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht01', 'htn01', 'sla01');
+VALUES ('ht01', 'Homo Erectus', '');
+
+--mesolitik
+
+
+--neolitik
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht03', 'Homo Floresiensis', '');
