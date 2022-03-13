ab6bfa117e453fe97eb4428220e533ee55a5affd
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-13 22:29:22 +0800

    Update timeperiod.sql

--- timeperiod.sql
+++ timeperiod.sql
@@ -1,2 +1,8 @@
 INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
-VALUES ('tp01', , );
+VALUES ('tp01', 40000, 12000);
+
+INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
+VALUES ('tp01', 12000, 2800);
+
+INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
+VALUES ('tp01', 2800, 500);
