1658502830084ea70c3a7339b90ca044832b0ad4
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-13 17:08:23 +0800

    Update table.sql

--- table.sql
+++ table.sql
@@ -8,10 +8,10 @@ CREATE TABLE Time_Period(
 
 CREATE TABLE Invented_In(
 	InventedTimeID VARCHAR(255),
-	InventedTechnologyID VARCHAR(255)
+	InventedTechnologyID VARCHAR(255),
 	CONSTRAINT FK_InventedTime FOREIGN KEY (InventedTimeID)
 	REFERENCES Time_Period (TimeID),
-	CONSTRAINT FK_InventedTechnologyID FOREIGN KEY (InventedTechnologyID)
+	CONSTRAINT FK_InventedTechnology FOREIGN KEY (InventedTechnologyID)
 	REFERENCES Technology(TechnologyID)
 );
 
@@ -23,15 +23,20 @@ CREATE TABLE Technology(
 );
 
 CREATE TABLE Used_Technology(
-	TechnologyID VARCHAR(255),
-	Human_TypeID VARCHAR(255)
+	UsedTechnologyID VARCHAR(255),
+	Human_UsedID VARCHAR(255),
+	CONSTRAINT FK_UsedTechnology FOREIGN KEY (UsedTechnologyID)
+	REFERENCES Technology(TechnologyID),
+	CONSTRAINT FK_Human_Used FOREIGN KEY (Human_UsedID)
+	REFERENCES Human_Type(Human_TypeID)
 );
 
 CREATE TABLE Human_Type(
 	Human_TypeID VARCHAR(255) PRIMARY KEY NOT NULL,
-	TimeID VARCHAR(255),
 	Human_Type_Name VARCHAR(255),
-	S_L_AreaID VARCHAR(255)
+	Human_AreaID VARCHAR(255),
+	CONSTRAINT FK_Human_Area FOREIGN KEY (Human_AreaID)
+	REFERENCES Small_Local_Area(S_L_AreaID)
 );
 
 CREATE TABLE Local_Area (
@@ -41,14 +46,26 @@ CREATE TABLE Local_Area (
 
 CREATE TABLE Small_Local_Area (
 	S_L_AreaID VARCHAR(255) NOT NULL PRIMARY KEY,
-	L_AreaID VARCHAR(255),
-	District_Name VARCHAR(255)
+	Main_AreaID VARCHAR(255),
+	District_Name VARCHAR(255),
+	CONSTRAINT FK_Main_Area FOREIGN KEY (Main_AreaID)
+	REFERENCES Local_Area(L_AreaID)
 );
 
-CREATE TABLE Area_On_Time(
-	TimeID VARCHAR(255),
-	S_L_AreaID VARCHAR(255)
+CREATE TABLE Area_Has_Time(
+	A_TimeID VARCHAR(255),
+	Area_HTID VARCHAR(255),
+	CONSTRAINT FK_A_Time FOREIGN KEY (A_TimeID)
+	REFERENCES Time_Period (TimeID),
+	CONSTRAINT FK_Area_HT FOREIGN KEY (Area_HTID)
+	REFERENCES Small_Local_Area(S_L_AreaID)
 );
 
-
-
+CREATE TABLE Existed_In(
+	Exist_TimeID VARCHAR(255),
+	Human_ExistID (255),
+	CONSTRAINT FK_ExistTime FOREIGN KEY (Exist_TimeID)
+	REFERENCES Time_Period (TimeID),
+	CONSTRAINT FK_Human_Exist FOREIGN KEY (Human_ExistID)
+	REFERENCES Human_Type(Human_TypeID)
+);
