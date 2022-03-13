5c25f7713ee02709cb01826193963e723b8f0b53
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-14 00:02:50 +0800

    Update technology.sql

--- technology.sql
+++ technology.sql
@@ -1,2 +1,38 @@
+--paleolitik
+
 INSERT INTO Technology (TechnologyID, Tools, Material)
-VALUES ('tn01', '', '');
+VALUES ('tn01', 'Chopper', 'stone');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn02', 'Flakes', 'stone');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn03', 'Cleaver', 'stone');
+
+
+--Mesolitik
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn04', 'Scrapers', 'flint');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn05', 'Microliths', 'stone');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn06', 'Burins', 'flint');
+
+--Neolitik
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn07', 'Pottery', 'clay');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn08', 'Adzes', 'stone');
+
+--bronze age
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn09', 'Axes', 'metal');
+
+INSERT INTO Technology (TechnologyID, Tools, Material)
+VALUES ('tn09', 'Adze', 'bronze');
