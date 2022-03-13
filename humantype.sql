e76ed06187bc490142c5ebe0f54faa2cf814d096
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-13 18:45:42 +0800

    Update humantype.sql

--- humantype.sql
+++ humantype.sql
@@ -1,2 +1,2 @@
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, Human_AreaID)
-VALUES ('ht01', 'htn01', 'hta01');
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'htn01', 'sla01');
