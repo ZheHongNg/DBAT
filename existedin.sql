9ef1c10b312cf1e5c940b8e8e7af8c16a439a9e7
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-15 01:01:20 +0800

    Update existedin.sql

--- existedin.sql
+++ existedin.sql
@@ -1,2 +1,55 @@
+--paleolitik
+
 INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
-VALUES ('extp01', 'exht01');
+VALUES ('tp01', 'ht01');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp01', 'ht02');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp01', 'ht03');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp01', 'ht04');
+
+
+--mesolitik
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp02', 'ht05');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp02', 'ht06');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp02', 'ht07');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp02', 'ht08');
+
+
+--neolitik
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp03', 'ht09');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp03', 'ht10');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp03', 'ht11');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp03', 'ht12');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp03', 'ht13');
+
+
+--metal age
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp04', 'ht14');
+
+INSERT INTO Existed_In (Exist_TimeID, Human_ExistID)
+VALUES ('tp04', 'ht15');
