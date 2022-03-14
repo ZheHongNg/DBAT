7485ff13195f6db41f5bc1110632b93f91ba4dbf
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-14 14:06:48 +0800

    Update localarea.sql

--- localarea.sql
+++ localarea.sql
@@ -1,2 +1,35 @@
+--p m n
+
 INSERT INTO Local_Area (L_AreaID, State_Name)
-VALUES ('la01', '');
+VALUES ('la01', 'Sabah');
+
+--p n
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la02', 'Sarawak');
+
+--p
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la03', 'Perak');
+
+--m n l
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la04', 'Selangor');
+
+--m
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la05', 'Kelantan');
+
+--m
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la06', 'Perlis');
+
+--n l
+
+INSERT INTO Local_Area (L_AreaID, State_Name)
+VALUES ('la07', 'Pahang');  
+
