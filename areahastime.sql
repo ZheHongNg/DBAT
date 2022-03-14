acd1bdca52d7c50b066946bd6835d0e541b690ee
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-15 00:51:28 +0800

    Update areahastime.sql

--- areahastime.sql
+++ areahastime.sql
@@ -1,2 +1,53 @@
+--paleolitik
+
 INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
-VALUES ('atp01', 'aht01');
+VALUES ('tp01', 'sla01');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp01', 'sla02');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp01', 'sla03');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp01', 'sla04');
+
+
+--mesolitik
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp02', 'sla05');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp02', 'sla06');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp02', 'sla07');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp02', 'sla08');
+
+--neolitik
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp03', 'sla09');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp03', 'sla10');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp03', 'sla11');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp03', 'sla12');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp03', 'sla13');
+
+--metal age
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp04', 'sla14');
+
+INSERT INTO Area_Has_Time (A_TimeID, Area_HTID)
+VALUES ('tp04', 'sla15');
