2f082d5e63f62a8ba64f962d4791b0e01ca98e4f
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-15 00:29:05 +0800

    Update usedtechnology.sql

--- usedtechnology.sql
+++ usedtechnology.sql
@@ -1,2 +1,112 @@
+--paleolitik
+
 INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
-VALUES ('utn01', 'uht01');
+VALUES ('tn01', 'ht01');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn03', 'ht01');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn01', 'ht02');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn02', 'ht02');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn03', 'ht02');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn01', 'ht03');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn02', 'ht03');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn03', 'ht03');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn02', 'ht04');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn03', 'ht04');
+
+
+--mesolitik
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn04', 'ht05');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn05', 'ht05');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn06', 'ht05');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn04', 'ht06');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn05', 'ht06');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn04', 'ht07');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn05', 'ht07');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn04', 'ht08');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn05', 'ht08');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn06', 'ht08');
+
+
+--neolitik
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn07', 'ht09');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn08', 'ht09');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn07', 'ht10');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn08', 'ht10');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn07', 'ht11');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn08', 'ht11');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn07', 'ht12');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn08', 'ht12');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn07', 'ht13');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn08', 'ht13');
+
+
+--metal age
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn09', 'ht14');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn10', 'ht14');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn09', 'ht15');
+
+INSERT INTO Used_Technology (UsedTechnologyID, Human_UsedID)
+VALUES ('tn10', 'ht15');
