a21add0ea2a2b8f72ccf9839d45a4d4859a8f159
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-14 20:54:19 +0800

    Update humantype.sql

--- humantype.sql
+++ humantype.sql
@@ -1,19 +1,52 @@
 --paleolitik
 
 INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht01', 'Homo Erectus', '');
+VALUES ('ht01', 'Homo Erectus', 'sla01');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Erectus', 'sla02');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Erectus', 'sla03');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Erectus', 'sla04');
 
 --mesolitik
 
 INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht01', 'Homo Soloensis', '');
+VALUES ('ht01', 'Homo Soloensis', 'sla05');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Soloensis', 'sla06');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Soloensis', 'sla07');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Soloensis', 'sla08');
 
 --neolitik
 
 INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht03', 'Homo Floresiensis', '');
+VALUES ('ht03', 'Homo Floresiensis', 'sla09');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht03', 'Homo Floresiensis', 'sla10');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht03', 'Homo Floresiensis', 'sla11');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht03', 'Homo Floresiensis', 'sla12');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht03', 'Homo Floresiensis', 'sla13');
 
 --logam
 
 INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht01', 'Homo Sapien', '');
+VALUES ('ht01', 'Homo Sapien', 'sla14');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht01', 'Homo Sapien', 'sla15');
