89ff2ed9fd983afb83f35ac84a8816f731014475
FaizRahman01 <52125715+FaizRahman01@users.noreply.github.com>
2022-03-14 23:36:04 +0800

    Update inventedin.sql

--- inventedin.sql
+++ inventedin.sql
@@ -1,2 +1,37 @@
+--paleolitik
+
 INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
-VALUES ('itp01', 'itn01');
+VALUES ('tp01', 'tn01');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp01', 'tn02');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp01', 'tn03');
+
+--mesolitik
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp02', 'tn04');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp02', 'tn05');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp02', 'tn06');
+
+--Neolitik
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp03', 'tn07');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp03', 'tn08');
+
+--metal age
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp04', 'tn09');
+
+INSERT INTO Invented_In (InventedTimeID, InventedTechnologyID)
+VALUES ('tp04', 'tn10');
