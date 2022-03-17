18582ac157a45e598ac272c1bdea38748f7e87fe
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-17 17:07:58 +0800

    Add files via upload

---
+++ human_living.sql
@@ -0,0 +1,52 @@
+-paleolitik
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht01', 'sla01');
+
+INSERT INTO Human_Type (Human_TypeID,  S_L_AreaID)
+VALUES ('ht01', 'sla02');
+
+INSERT INTO Human_Type (Human_TypeID,  S_L_AreaID)
+VALUES ('ht01', 'sla03');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht01', 'sla04');
+
+--mesolitik
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht02', 'sla05');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht02', 'sla06');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht02', 'sla07');
+
+INSERT INTO Human_Type (Human_TypeID,  S_L_AreaID)
+VALUES ('ht02', 'sla08');
+
+--neolitik
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht03','sla09');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht03', 'sla10');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht03', 'sla11');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht03', 'sla12');
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht03', 'sla13');
+
+--logam
+
+INSERT INTO Human_Type (Human_TypeID, S_L_AreaID)
+VALUES ('ht04', 'sla14');
+
+INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
+VALUES ('ht04', 'sla15');
