038f447c587e47b4af1f42f187d17de1fa446c04
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-17 21:50:27 +0800

    Update timeperiod.sql

--- timeperiod.sql
+++ timeperiod.sql
@@ -1,19 +1,19 @@
 --paleolitik
 
 INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
-VALUES ('tp01', 40000, 35000);
+VALUES ('tp01', '40000BC', '35000BC');
 
 --mesolitik
 
 INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
-VALUES ('tp02', 35000, 10000);
+VALUES ('tp02', '12000BC', '8000BC');
 
 --neolitik
 
 INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
-VALUES ('tp03', 10000, 5000);
+VALUES ('tp03', '2800BC', '500BC');
 
---iron age
+--bronze age
 
 INSERT INTO Time_Period (TimeID, YearStart, YearEnd)
-VALUES ('tp04', 5000, 2500);
+VALUES ('tp04', '500BC', '500CE');
