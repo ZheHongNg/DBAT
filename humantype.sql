8e0853e3d1a4feed288eab84c1b7726b86ae35af
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-17 16:30:28 +0800

    Update humantype.sql

--- humantype.sql
+++ humantype.sql
@@ -1,52 +1,19 @@
 --paleolitik
 
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht01', 'Homo Erectus', 'sla01');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht02', 'Homo Erectus', 'sla02');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht03', 'Homo Erectus', 'sla03');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht04', 'Homo Erectus', 'sla04');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name)
+VALUES ('ht01', 'Homo erestus');
 
 --mesolitik
 
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht05', 'Homo Soloensis', 'sla05');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht06', 'Homo Soloensis', 'sla06');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht07', 'Homo Soloensis', 'sla07');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht08', 'Homo Soloensis', 'sla08');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name)
+VALUES ('ht02', 'Homo Soloensis');
 
 --neolitik
 
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht09', 'Homo Floresiensis', 'sla09');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht10', 'Homo Floresiensis', 'sla10');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht11', 'Homo Floresiensis', 'sla11');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht12', 'Homo Floresiensis', 'sla12');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht13', 'Homo Floresiensis', 'sla13');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name)
+VALUES ('HT03', 'Homo Floresisensis');
 
 --logam
 
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht14', 'Homo Sapien', 'sla14');
-
-INSERT INTO Human_Type (Human_TypeID, Human_Type_Name, S_L_AreaID)
-VALUES ('ht15', 'Homo Sapien', 'sla15');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name)
+VALUES ('HT04', 'Homo Sapien');
