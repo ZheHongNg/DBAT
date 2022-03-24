21e37a729b1515610c1fd56b8918c9ccf6a1d80c
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-24 22:52:04 +0800

    Update smalllocalarea.sql

--- smalllocalarea.sql
+++ smalllocalarea.sql
@@ -1,55 +1,55 @@
 --paleolitik
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla01', 'la01', 'Tingkayu');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla01', 'la01', 'ht01', 'Tingkayu');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla02', 'la02', 'Gua Niah');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla02', 'la02', 'ht01', 'Gua Niah');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla03', 'la03', 'Kota Tampan');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla03', 'la03', 'ht01', 'Kota Tampan');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla04', 'la01', 'Lembah Mansuli');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla04', 'la01', 'ht01', 'Lembah Mansuli');
 
 
 --mesolitik
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla05', 'la04', 'Jenderam Hilir');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla05', 'la04', 'ht02', 'Jenderam Hilir');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla06', 'la01', 'Gua Madai');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla06', 'la01', 'ht02', 'Gua Madai');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla07', 'la05', 'Gua Cha');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla07', 'la05', 'ht02', Gua Cha');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla08', 'la06', 'Bukit Tegun Lembu');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla08', 'la06', 'ht02', 'Bukit Tegun Lembu');
 
 
 --neolitik
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla09', 'la07', 'Gua Kecil');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla09', 'la07', 'ht03','Gua Kecil');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla10', 'la07', 'Sungai Tembeling');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla10', 'la07', 'ht03','Sungai Tembeling');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla11', 'la02', 'Lubang Angin');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla11', 'la02', 'ht03', 'Lubang Angin');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla12', 'la01', 'Gua Tapadong');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla12', 'la01', 'ht03','Gua Tapadong');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla13', 'la04', 'Jenderam Hilir');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla13', 'la04', 'ht03','Jenderam Hilir');
 
 
 --logam
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla14', 'la07', 'Sungai Tembeling');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla14', 'la07', 'ht04', 'Sungai Tembeling');
 
-INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, District_Name)
-VALUES ('sla15', 'la04', 'Lembah Bernam');
+INSERT INTO Small_Local_Area (S_L_AreaID, L_AreaID, Human_TypeID, District_Name)
+VALUES ('sla15', 'la04', 'ht04', 'Lembah Bernam');
