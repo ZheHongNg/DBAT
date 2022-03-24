b03e80fe801d4ade419d5a96a9ab80a837ae7d1b
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-24 18:47:40 +0800

    Update humantype.sql

--- humantype.sql
+++ humantype.sql
@@ -1,19 +1,19 @@
 --paleolitik
 
-INSERT INTO human_type (Human_TypeID, Human_Type_Name)
-VALUES ('ht01', 'Homo erestus');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name, TimeID)
+VALUES ('ht01', 'Homo erestus', 'tp01');
 
 --mesolitik
 
-INSERT INTO human_type (Human_TypeID, Human_Type_Name)
-VALUES ('ht02', 'Homo Soloensis');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name, TimeID)
+VALUES ('ht02', 'Homo Soloensis', 'tp02');
 
 --neolitik
 
-INSERT INTO human_type (Human_TypeID, Human_Type_Name)
-VALUES ('ht03', 'Homo Floresisensis');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name, TimeID)
+VALUES ('ht03', 'Homo Floresisensis', 'tp03');
 
 --logam
 
-INSERT INTO human_type (Human_TypeID, Human_Type_Name)
-VALUES ('ht04', 'Homo Sapien');
+INSERT INTO human_type (Human_TypeID, Human_Type_Name, TimeID)
+VALUES ('ht04', 'Homo Sapien', 'tp04');
