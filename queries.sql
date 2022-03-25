14c80b5292e770f20909dc73603e5a6038d16ec7
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-25 16:23:33 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -10,7 +10,7 @@ FROM small_local_area
 join local_area on small_local_area.L_AreaID = local_area.L_AreaID
 join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID
 join time_period on Human_Type.TimeID = time_period.TimeID
-join used_technology on human_type.Human_TypeID = used_technology.Human_TypeID
-join technology on technology.TechnologyID = used_technology.TechnologyID;
+join used_technology on human_type.Human_TypeID = used_technology.Human_UsedID
+join technology on technology.TechnologyID = used_technology.UsedTechnologyID;
 
 B4
