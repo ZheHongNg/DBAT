9b6b1b33c491fd69968a8fadb1398d6fd6c0c00e
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-25 17:54:57 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,15 +7,13 @@ JOIN Human_Type ON small_local_area.Human_TypeID = Human_Type.Human_TypeID;
 B2
 
 B3
-SELECT small_local_area.District_name, time_period.PeriodName, 
-
 SELECT DISTINCT local_area.State_Name AS State, COUNT(used_technology.UsedTechnologyID) AS Technology_Used, time_period.PeriodName AS Age
 FROM small_local_area
 join local_area on small_local_area.L_AreaID = local_area.L_AreaID
 join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID
 join time_period on Human_Type.TimeID = time_period.TimeID
 join used_technology on human_type.Human_TypeID = used_technology.Human_UsedID
-GROUP BY State, Age
-HAVING COUNT(used_technology.UsedTechnologyID)>0;
+GROUP BY local_area.State_Name, time_period.PeriodName
+HAVING COUNT(used_technology.UsedTechnologyID);
 
 B4
