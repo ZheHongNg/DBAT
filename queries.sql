0ef8cc84b6aad959c467ecd7078e03c4399c3733
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-25 16:19:40 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -7,10 +7,10 @@ SELECT small_local_area.District_name, time_period.PeriodName,
 
 SELECT *
 FROM small_local_area
-join local_area on small_local_area.L_AreaID = local_area.L_AreaID,
-join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID,
-join time_period on Human_Type.TimeID = time_period.TimeID,
-join used_technology on human_type.Human_TypeID = used_technology.Human_TypeID,
+join local_area on small_local_area.L_AreaID = local_area.L_AreaID
+join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID
+join time_period on Human_Type.TimeID = time_period.TimeID
+join used_technology on human_type.Human_TypeID = used_technology.Human_TypeID
 join technology on technology.TechnologyID = used_technology.TechnologyID;
 
 B4
