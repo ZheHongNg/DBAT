b9a2c77a4f4cee6425f4b025e6813e82baed84c4
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-25 15:21:40 +0800

    Add files via upload

---
+++ subtables.sql
@@ -0,0 +1,11 @@
+INSERT INTO StoneAge (Cat_StoneID, Description)
+VALUES ('tp01', 'Old Stone Age,  is a period in prehistory distinguished by the original development of stone tools that covers c. 99% of the period of human technological prehistory.');
+
+INSERT INTO StoneAge (Cat_StoneID, Description)
+VALUES ('tp02', 'This age is also called Epipaleolithic, is the Old World archaeological period between the Paleolithic and the Neolithic. ');
+
+INSERT INTO StoneAge (Cat_StoneID, Description)
+VALUES ('tp03', ' The period is final division of the Stone Age, , marked by the development of metallurgy, leading up to the Bronze Age and Iron Age.');
+
+INSERT INTO MetalAge (Cat_MetalID, Description)
+VALUES ('tp04', 'This age is a historic period, approximately 3300 BC to 1200 BC, that was characterized by the use of bronze, in some areas proto-writing, and other early features of urban civilization. ');
