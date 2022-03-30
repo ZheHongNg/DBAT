16a1d4e57bbffb45ea12f96d19617b5ff2696905
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-30 16:56:27 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -29,7 +29,7 @@ join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID
 join time_period on Human_Type.TimeID = time_period.TimeID
 join used_technology on human_type.Human_TypeID = used_technology.Human_UsedID
 join technology on technology.TechnologyID = used_technology.UsedTechnologyID
-GROUP BY technology.Tools)
+GROUP BY technology.Tools, time_period.TimeID)
 ORDER BY Age;
 
 SELECT technology.Tools AS Tech_Name,
