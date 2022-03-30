ef37ffaafb313f890977bb0f28a2696b6bedef85
ZheHongNg <91187022+ZheHongNg@users.noreply.github.com>
2022-03-30 16:06:51 +0800

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -22,10 +22,9 @@ GROUP BY local_area.State_Name, time_period.PeriodName
 HAVING COUNT(used_technology.UsedTechnologyID)>0;
 
 B4
-SELECT B_summary.Tech_Name, B_summary.count_LocalArea, time_period.Age
-FROM (SELECT COUNT(DISTINCT local_area.State_Name) AS count_LocalArea, technology.Tools AS Tech_Name
+SELECT B_summary.Tech_Name, B_summary.count_LocalArea, time_period.
+FROM (SELECT COUNT(DISTINCT local_area.L_AreaID) AS count_LocalArea, technology.Tools AS Tech_Name
 FROM small_local_area
-join local_area on small_local_area.L_AreaID = local_area.L_AreaID
 join Human_Type on small_local_area.Human_TypeID = Human_Type.Human_TypeID
 join used_technology on human_type.Human_TypeID = used_technology.Human_UsedID
 join technology on technology.TechnologyID = used_technology.UsedTechnologyID
